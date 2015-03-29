13cdc119446b02ebc1203fd667384e15e7d52354
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2015-03-29 16:56:46 +0000

    preparation for release 1.5

---
+++ tags/newdir/src/buildtag.hh
@@ -0,0 +1,3 @@
+#include "Common.hh"
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 9522", "2015-03-29 14:17:38 UTC", "Linux 3.13.0-37-generic i686", "'--enable-maintainer-mode' 'DEVELOP_WANTED=yes'"
+#define ARCHIVE_SVN  9479
